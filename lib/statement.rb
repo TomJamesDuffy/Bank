d919861b2602330638d02ca97294419f7eaac065
Thomas Duffy <tomjamesduffy@gmail.com>
2018-04-23 14:17:54 +0100

    TDD adjusted print from return value to printed output

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,12 +1,11 @@
 # Prints all transactions in transaction history
 class Statement
   def self.print_transactions(transaction_history)
-    string = 'date || credit || debit || balance'
+    print "date || credit || debit || balance"
     transaction_history.each_with_index do |t, i|
-      string += "\n#{t.created_date} || #{t.credit} "\
+      print "\n#{t.created_date} || #{t.credit} "\
                 "|| #{t.debit} || #{balance(transaction_history, i)}"
     end
-    return string
   end
 
   def self.balance(transaction_history, pos)
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -13,13 +13,15 @@ describe Statement do
 
   let(:dummy_bank) do
     double :bank,
-           transaction_history: [dummy_transaction]
+           transaction_history: [dummy_transaction, dummy_transaction]
   end
 
   describe '#print_transactions' do
     it 'prints the date, time and amount of each transaction' do
-      expect(statement.print_transactions(dummy_bank.transaction_history))
-        .to eq("date || credit || debit || balance\n19/11/1990 || 0 || 30 || 30")
+      expect{ statement.print_transactions(dummy_bank.transaction_history) }
+        .to output("date || credit || debit || balance\n"\
+               "19/11/1990 || 0 || 30 || 30\n"\
+               "19/11/1990 || 0 || 30 || 60").to_stdout
     end
   end
 end
